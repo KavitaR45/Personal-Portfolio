bc2ec89a70cbc197a0aa47f338740984b72df234
Kavita2599 <kavita99@netart.io>
2024-03-29 14:24:32 +0530

    testimonial carousel added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 import { HeroParallax } from "@/components/ui/hero-parallax";
 import { ModeToggle } from "@/components/ui/model-toggle";
 import { Button } from "@/components/ui/button"
+import {InfiniteMovingCards} from "@/components/ui/infinite-moving-cards";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import Image from "next/image";
 export const products = [
@@ -118,6 +119,37 @@ const words = [
     className: "text-blue-500 dark:text-blue-500",
   },
 ];
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
 export default function Home() {
 
   return (
@@ -132,10 +164,10 @@ export default function Home() {
               </h1>
               <TypewriterEffectSmooth words={words} />
               <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-                <Button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+                <Button className="w-40 h-10 rounded-xl bg-black border dark:border-white dark:text-white dark:hover:text-black border-transparent text-white text-sm">
                   Join now
                 </Button>
-                <Button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+                <Button variant="outline" className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
                   Signup
                 </Button>
               </div>
@@ -159,6 +191,13 @@ export default function Home() {
         />
       </ContainerScroll>
       <HeroParallax products={products} />
+      <div className="h-[20rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
+    </div>
     </>
   );
 }
