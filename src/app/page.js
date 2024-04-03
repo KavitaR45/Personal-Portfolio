ee6be8686d2ce46908c180451cf457ab5fbbac6c
@Kavita2599 <kavita99@netart.io>
2024-04-03 18:19:17 +0530

    portfolio section created

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,203 @@
 "use client";
 import Hero from "@/components/hero";
-
-;
+import LayoutGrid from "@/components/ui/layout-grid";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
+  const SkeletonOne = () => {
+    return (
+      <div>
+        <p className="font-bold text-4xl text-white">House in the woods</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+          A serene and tranquil retreat, this house in the woods offers a peaceful
+          escape from the hustle and bustle of city life.
+        </p>
+      </div>
+    );
+  };
 
+  const SkeletonTwo = () => {
+    return (
+      <div>
+        <p className="font-bold text-4xl text-white">House above the clouds</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+          Perched high above the world, this house offers breathtaking views and a
+          unique living experience. It&apos;s a place where the sky meets home,
+          and tranquility is a way of life.
+        </p>
+      </div>
+    );
+  };
+  const SkeletonThree = () => {
+    return (
+      <div>
+        <p className="font-bold text-4xl text-white">Greens all over</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+          A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
+          perfect place to relax, unwind, and enjoy life.
+        </p>
+      </div>
+    );
+  };
+  const SkeletonFour = () => {
+    return (
+      <div>
+        <p className="font-bold text-4xl text-white">Rivers are serene</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+          A house by the river is a place of peace and tranquility. It&apos;s the
+          perfect place to relax, unwind, and enjoy life.
+        </p>
+      </div>
+    );
+  };
+
+  const Wordpresscards = [
+    {
+      id: 1,
+      content: <SkeletonOne />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 3,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 5,
+      content: <SkeletonOne />,
+      className: "md:col-span-2",
+      thumbnail:
+        "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 6,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+
+  ];
+  const ReactJscards = [
+    {
+      id: 1,
+      content: <SkeletonOne />,
+      className: "col-span-1",
+      thumbnail:
+        "/temp-banner.png",
+    },
+    {
+      id: 2,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 3,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 4,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 5,
+      content: <SkeletonOne />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 6,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+
+  ];
+  const Othercards = [
+    {
+      id: 1,
+      content: <SkeletonOne />,
+      className: "md:col-span-2",
+      thumbnail:
+        "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 3,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 4,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 5,
+      content: <SkeletonOne />,
+      className: "col-span-1",
+      thumbnail:
+        "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
   return (
     <>
       <div className='min-h-full flex flex-col dark:bg-[#1F1F1F]'>
         <Hero />
+        {/* Portfolio Section */}
+        <div className="relative isolate">
+          <div className="mx-auto max-w-3xl py-10 sm:py-10 px-7">
+            <div className="text-center">
+              <h2 className="text-2xl md:text-4xl font-bold tracking-tight dark:text-white text-gray-900">Data to <span class="relative whitespace-nowrap text-blue-600"><svg aria-hidden="true" viewBox="0 0 418 42" class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70" preserveAspectRatio="none"><path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path></svg><span class="relative">enrich</span></span> online</h2>
+              <p className="mt-6 leading-7 dark:text-white text-gray-600">Empowering businesses with intuitive reporting solutions, our platform simplifies data analysis. Seamlessly generate customized reports, gaining actionable insights effortlessly.</p>
+            </div>
+          </div>
+        </div>
+        <Tabs defaultValue="Wordpress" className="block">
+          <TabsList className="w-max mx-auto block">
+            <TabsTrigger value="Wordpress">Wordpress</TabsTrigger>
+            <TabsTrigger value="React Js">React Js</TabsTrigger>
+            <TabsTrigger value="Other">Other</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Wordpress"><LayoutGrid cards={Wordpresscards} /></TabsContent>
+          <TabsContent value="React Js"> <LayoutGrid cards={ReactJscards} /></TabsContent>
+          <TabsContent value="Other"> <LayoutGrid cards={Othercards} /></TabsContent>
+        </Tabs>
+        {/* Portfolio Section */}
       </div>
     </>
   );
--- src/components/hero.js
+++ src/components/hero.js
@@ -9,8 +9,8 @@ export default function Hero() {
         <>
             <div className="flex flex-col lg:flex-row justify-start items-center pt-10 pb-10 md:pt-16 md:pb-16 gap-4 px-7 md:px-10">
                 <div className="flex flex-col w-full lg:w-1/2 lg:pr-20">
-                    <h1 className='text-3xl md:text-5xl font-bold mt-2 mb-2'>Empowering Brands through Engaging Web Experiences</h1>
-                    <p className='font-normal mt-2 mb-2'>Step into a world where innovation and creativity converge to redefine digital landscapes. Explore the dynamic portfolio of a Front-End Developer committed to crafting captivating web experiences. </p>
+                    <h1 className='text-3xl  md:text-5xl font-bold mt-2 mb-2'>Empowering Brands through Engaging Web Experiences</h1>
+                    <p className='leading-7 font-normal mt-2 mb-2'>Step into a world where innovation and creativity converge to redefine digital landscapes. Explore the dynamic portfolio of a Front-End Developer committed to crafting captivating web experiences. </p>
                     <Button className='w-fit mt-4 mb-2'>Download Resume</Button>
                 </div>
                 <div className="flex justify-end relative lg:w-1/2">
